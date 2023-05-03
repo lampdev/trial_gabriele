70e4f4a8d44f96878cec6a33b282cb3801953bfb
lampdev <orders@lamp-dev.com>
2023-05-03 13:20:34 +0300

    Service for fetching data from CoinGecko

---
+++ app/Services/CoinGeckoAPIService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+
+class CoinGeckoAPIService
+{
+    protected const API_KEY_HEADER_ATTRIBUTE_NAME = 'x-cg-pro-api-key';
+
+    protected const INCLUDE_PLATFORM_REQUEST_ATTRIBUTE_NAME = 'include_platform';
+
+    protected const DEFAULT_ERROR_MESSAGE = 'Something went wrong.';
+
+    protected const COINS_LIST_PATH = '/coins/list';
+
+    protected string $defaultURL;
+
+    protected ?string $apiKey = null;
+
+    public function __construct()
+    {
+        $this->defaultURL = config('coin_gecko.url');
+        $this->apiKey = config('coin_gecko.api_key');
+    }
+
+    /**
+     * Get error message from response or return default one.
+     */
+    protected function getResponseErrorMessage(Response $response): string
+    {
+        return $response->json('status.error_message') ?? self::DEFAULT_ERROR_MESSAGE;
+    }
+
+    /**
+     * Return PendingRequest with set up headers.
+     */
+    protected function getHttpClient(): PendingRequest
+    {
+        $client = Http::acceptJson();
+
+        if ($this->apiKey ?? false) {
+            $client->withHeaders([self::API_KEY_HEADER_ATTRIBUTE_NAME => $this->apiKey]);
+        }
+
+        return $client;
+    }
+
+    /**
+     * Fetch coins data. Throw Exception if error.
+     *
+     * @param  bool|null  $includePlatform flag to include platform contract addresses
+     *
+     * @throws Exception
+     */
+    public function getCoinsList(?bool $includePlatform = false): array
+    {
+        $response = $this->getHttpClient()
+            ->get($this->defaultURL.self::COINS_LIST_PATH, [
+                self::INCLUDE_PLATFORM_REQUEST_ATTRIBUTE_NAME => $includePlatform ? 'true' : 'false',
+            ]);
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        throw new Exception($this->getResponseErrorMessage($response));
+    }
+}
---
+++ config/coin_gecko.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'url' => 'https://api.coingecko.com/api/v3',
+    'api_key' => env('COIN_GECKO_API_KEY'),
+];
